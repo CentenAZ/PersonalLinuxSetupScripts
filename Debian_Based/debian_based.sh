974aa395089c3094b74b265ce2819413db35a6a1
CentenAZ <58229529+CentenAZ@users.noreply.github.com>
2021-04-06 18:43:09 -0600

    Debian - Brave Install Patch

--- Debian_Based/debian_based.sh
+++ Debian_Based/debian_based.sh
@@ -7,9 +7,10 @@
 sudo apt update && upgrade -y
 
 # Install Brave Browser
-sudo apt install apt-transport-https curl
+sudo apt install apt-transport-https curl -y
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+sudo apt update -y
 sudo apt install brave-browser -y
 
 # OPTIONAL APPLICATIONS
