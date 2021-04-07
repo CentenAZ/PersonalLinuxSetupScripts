d37cf5d51eab8c63a58879844de1c16396432459
CentenAZ <58229529+CentenAZ@users.noreply.github.com>
2021-04-06 18:54:16 -0600

    Create debian_vboxguestadd.sh

---
+++ Debian_Based/debian_vboxguestadd.sh
@@ -0,0 +1,6 @@
+sudo apt update
+sudo apt install build-essential dkms linux-headers-$(uname -r)
+sudo mkdir -p /mnt/cdrom
+sudo mount /dev/cdrom /mnt/cdrom
+cd /mnt/cdrom
+sudo sh ./VBoxLinuxAdditions.run --nox11
