86bafa0ec789b5b11f9a476184e9dccbc8f0f698
CentenAZ <58229529+CentenAZ@users.noreply.github.com>
2021-04-30 18:10:22 -0600

    Create rpm_vboxguestadd.sh

---
+++ RPM_Based/rpm_vboxguestadd.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+###################################################
+# RPM BASED DISTROS
+###################################################
+
+
+# Update Software
+sudo dnf update -y
+sudo yum update -y
+
+# Install Brave Browser
+sudo dnf install dnf-plugins-core -y
+sudo dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/x86_64/
+sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
+sudo dnf install brave-browser
+
+# Install OpenSSH Host
+sudo dnf install openssh-server -y
+
+# Install VIM
+sudo dnf install vim -y
+
+# OPTIONAL APPLICATIONS
+sudo dnf install neofetch -y
+sudo dnf install make -y
+
+mkdir /tmp
+cd /tmp
+git clone https://github.com/aristocratos/bashtop.git
+cd bashtop
+sudo make install -y
