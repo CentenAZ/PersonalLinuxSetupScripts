01d933bc9704b3d005a4dd56e4cd5e61cbb86c5d
Xisting Studios <xisting.studios@gmail.com>
2021-04-30 22:08:22 -0600

    Added Virtualbox Addons Install, Patched message prompts

--- RPM_Based/rpm_based.sh
+++ RPM_Based/rpm_based.sh
@@ -7,12 +7,13 @@
 # Update Software
 sudo dnf update -y
 sudo yum update -y
+sudo install epel-release -y
 
 # Install Brave Browser
 sudo dnf install dnf-plugins-core -y
-sudo dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/x86_64/
+sudo dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/x86_64/ -y
 sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
-sudo dnf install brave-browser
+sudo dnf install brave-browser -y
 
 # Install OpenSSH Host
 sudo dnf install openssh-server -y
@@ -28,4 +29,4 @@ mkdir /tmp
 cd /tmp
 git clone https://github.com/aristocratos/bashtop.git
 cd bashtop
-sudo make install -y
+sudo make install
