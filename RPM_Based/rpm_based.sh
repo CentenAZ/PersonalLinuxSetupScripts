63b103d745b71c8dc3e5bc84984bcd421b55ce6c
CentenAZ <58229529+CentenAZ@users.noreply.github.com>
2021-04-30 22:41:40 -0600

    Removed non-critical error in Brave Install

--- RPM_Based/rpm_based.sh
+++ RPM_Based/rpm_based.sh
@@ -11,7 +11,7 @@ sudo install epel-release -y
 
 # Install Brave Browser
 sudo dnf install dnf-plugins-core -y
-sudo dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/x86_64/ -y
+sudo dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/x86_64/
 sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
 sudo dnf install brave-browser -y
 
