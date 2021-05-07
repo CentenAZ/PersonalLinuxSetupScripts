ffeb47f00ad42b34b7c281b17596b3cf75c5b363
Vincent Nguyen <vincent.nguyennf@gmail.com>
2021-05-07 10:32:05 -0600

    Fix For Errors in RPM_Based

--- RPM_Based/rpm_based.sh
+++ RPM_Based/rpm_based.sh
@@ -6,34 +6,34 @@
 
 echo -e "\n\n"
 # Update Software
-echo -e "\nUpdating All Software"
+echo -e "Updating All Software"
 sudo dnf update -y
 sudo yum update -y
-echo -e "\nInstalling Extra Packages for Enterprise Linux"
+echo -e "Installing Extra Packages for Enterprise Linux"
 sudo dnf install epel-release -y
 
 # Install Brave Browser
-echo -e "\nInstalling The Brave Browser"
+echo -e "Installing The Brave Browser"
 sudo dnf install dnf-plugins-core -y
 sudo dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/x86_64/
 sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
 sudo dnf install brave-browser -y
 
 # Install OpenSSH Host
-echo -e "\nInstalling OpenSSH Server/Host"
+echo -e "Installing OpenSSH Server"
 sudo dnf install openssh-server -y
 
 # Install VIM
-echo -e "\nInstalling VIM"
+echo -e "Installing VIM"
 sudo dnf install vim -y
 
 # OPTIONAL APPLICATIONS
-echo -e "\nInstalling Neofetch"
+echo -e "Installing Neofetch"
 sudo dnf install neofetch -y
-echo -e "\nInstalling Make"
+echo -e "Installing Make"
 sudo dnf install make -y
 
-echo -e "\nInstalling Bashtop"
+echo -e "Installing Bashtop"
 mkdir /tmp
 cd /tmp
 git clone https://github.com/aristocratos/bashtop.git
