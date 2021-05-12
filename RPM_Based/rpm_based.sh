0e5832f1f95e2b2a691e80f4d6f8cc840e934669
Vincent Nguyen <vincent.nguyennf@gmail.com>
2021-05-11 18:02:47 -0600

    Added Formatting to Messages

--- RPM_Based/rpm_based.sh
+++ RPM_Based/rpm_based.sh
@@ -5,35 +5,34 @@
 
 
 # Update Software
-echo -e "\n\nUpdating All Software"
+echo -e "\n\n**Updating All Software**"
 sudo dnf update -y
 sudo yum update -y
-echo -e "\nInstalling Extra Packages for Enterprise Linux"
+echo -e "\n**Installing Extra Packages for Enterprise Linux**"
 sudo dnf install epel-release -y
 
 # Install Brave Browser
-echo -e "\nInstalling The Brave Browser"
+echo -e "\n**Installing The Brave Browser**"
 sudo dnf install dnf-plugins-core -y
 sudo dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/x86_64/
 sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
 sudo dnf install brave-browser -y
 
 # Install OpenSSH Host
-echo -e "\nInstalling OpenSSH Server"
+echo -e "\n**Installing OpenSSH Server**"
 sudo dnf install openssh-server -y
 
 # Install VIM
-echo -e "\nInstalling VIM"
+echo -e "\n**Installing VIM**"
 sudo dnf install vim -y
 
 # OPTIONAL APPLICATIONS
-echo -e "\nInstalling Neofetch"
+echo -e "\n**Installing Neofetch**"
 sudo dnf install neofetch -y
-echo -e "\nInstalling Make"
+echo -e "\n**Installing Make**"
 sudo dnf install make -y
 
-echo -e "\nInstalling Bashtop"
-mkdir /tmp
+echo -e "\n**Installing Bashtop**"
 cd /tmp
 git clone https://github.com/aristocratos/bashtop.git
 cd bashtop
