1ccf25117980da0de2ce6948c32443568f2d32b5
Vincent Nguyen <vincent.nguyennf@gmail.com>
2021-05-19 15:25:54 -0600

    Added Status Comments into Guest Additions - RPM

--- RPM_Based/rpm_vboxguestadd.sh
+++ RPM_Based/rpm_vboxguestadd.sh
@@ -13,11 +13,15 @@ sleep 1
 echo -e "MAKE SURE THAT THE VIRTUALBOX GUEST ADDITIONS DISK IMAGE IS INSERTED INTO THE VM \n\n\n"
 sleep 2
 
+echo -e "\n\n**Updating All Software**"
 sudo dnf update -y
+echo -e "\n**Installing Extra Packages for Enterprise Linux**"
 sudo dnf install epel-release -y
 rpm -q epel-release
+echo -e "\n**Installing Required Packages For Virtualbox Guest Additions**"
 sudo dnf install gcc make perl kernel-devel kernel-headers bzip2 dkms -y
 
+echo -e "\n**Mounting and Executing Virtualbox Guest Additions Setup Disc**"
 sudo mkdir -p /mnt/vboxadd
 sudo mount /dev/cdrom /mnt/vboxadd
 cd /mnt/vboxadd
