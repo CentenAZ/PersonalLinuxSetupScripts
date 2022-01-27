cb4d3dd5c034128d8c038563cb3d9ce246ed171f
Vincent Nguyen <vincent.nguyennf@gmail.com>
2022-01-26 18:07:49 -0700

    Creating Root Patch

--- ArchLinux/createroot.sh
+++ ArchLinux/createroot.sh
@@ -3,9 +3,11 @@
 #//////////////////////////////////////////////////////
 
 
-echo -e "\n\nCREATING A USER IN ARCH LINUX.\nEnter the name for the user: "
-read $user
+echo -e "\n\nCREATING A USER IN ARCH LINUX."
+echo -e "\nInstalling sudo"
 pacman -S sudo --noconfirm
+echo "\n\nEnter the name for the user: "
+read user
 useradd --create-home --badnames $user
 echo -e "\nSet the password for the new root user:"
 passwd $user
