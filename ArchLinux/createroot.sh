fea87dc44fe0d20f45a1d25b9b1a4707f0a4e6ad
Vincent Nguyen <vincent.nguyennf@gmail.com>
2022-01-26 17:59:25 -0700

    Creating Root - Formatting

--- ArchLinux/createroot.sh
+++ ArchLinux/createroot.sh
@@ -3,11 +3,11 @@
 #//////////////////////////////////////////////////////
 
 
-echo "Creating a user in Arch Linux.\nEnter the name for the user: "
+echo -e "\n\nCREATING A USER IN ARCH LINUX.\nEnter the name for the user: "
 read $user
 pacman -S sudo --noconfirm
 useradd --create-home --badname $user
-echo "Set the password for the new root user:"
+echo -e "\nSet the password for the new root user:"
 passwd $user
 usermod --append --groups wheel $user
 sed -i 's/#\ %wheel\ ALL=(ALL)\ ALL/%wheel\ ALL=(ALL)\ ALL/' /etc/sudoers
