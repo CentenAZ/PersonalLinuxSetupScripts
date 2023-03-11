044c09899e109166ba441377894d6703281292be
Vincent Nguyen <vincent.nguyennf@gmail.com>
2023-03-10 18:40:22 -0700

    Fixed drive extending and changing to zsh

--- CustomFedora/setup.sh
+++ CustomFedora/setup.sh
@@ -18,19 +18,20 @@ sudo dnf group install "Hardware Support" -y
 
 # Expanding the partition
 echo "-----EXPANDING ROOT PARTITION-----"
-sudo lvextend -l+100%FREE /dev/mapper/fedora*
+sudo lvextend -r -l +100%FREE /dev/mapper/fedora*
 
 
 # Installing and changing default shell to zsh
 echo "-----INSTALLING AND CHANGING DEFAULT SHELL TO ZSH AND CUSTOMIZING IT-----"
 sudo dnf install zsh util-linux-user -y
-sudo chsh -s $(which zsh)
+sudo chsh -s $(which zsh) $()
 
 ################ post installation not ready yet
 mkdir ~/.zsh
 cp ~/.zshrc ~/.zshrc.bak
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"     # Installing Oh My Zsh
 # themes: jonathan, gnzh, agnoster
+# gnome extensions: blur my shell, dash to dock, extension list, vitals
 
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions       # Auto-suggestion plugin
 echo "source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh" | tee -a ~/.zshrc
